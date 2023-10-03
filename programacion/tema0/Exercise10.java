1264bcb9f01c484ddbd0ca26b475f91ed9171f1e
david <david.mallol@iesalvarofalomir.org>
2023-10-03 19:20:10 +0200

    Use primitive types

--- programacion/tema0/Exercise10.java
+++ programacion/tema0/Exercise10.java
@@ -6,9 +6,9 @@ public class Exercise10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Escribe el primer número: ");
-        Double number1 = scanner.nextDouble();
+        double number1 = scanner.nextDouble();
         System.out.print("Escribe el segundo número: ");
-        Double number2 = scanner.nextDouble();
+        double number2 = scanner.nextDouble();
         System.out.println("Suma: " + (number1 + number2));
         System.out.println("Resta: " + (number1 - number2));
         System.out.println("Multiplicación: " + (number1 * number2));
--- programacion/tema0/Exercise6.java
+++ programacion/tema0/Exercise6.java
@@ -6,8 +6,8 @@ public class Exercise6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Escribe el valor del radio: ");
-        Double radius = scanner.nextDouble();
-        Double area = 3.14 * radius * radius;
+        double radius = scanner.nextDouble();
+        double area = 3.14 * radius * radius;
         //Double area = Math.PI * Math.pow(radius, 2);
         System.out.println("El área es " + area);
     }
